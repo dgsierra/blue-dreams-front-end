7e8a07ee12c800222e2ba71679ffd81d59ce6576
Diego <godie8a@hotmail.com>
2023-01-18 10:20:40 -0500

    fix linters

---
+++ src/components/PrivateText.js
@@ -0,0 +1,29 @@
+import { useState, useEffect } from 'react';
+
+const PrivateText = ({ currUser }) => {
+  const [message, setMessage] = useState(null);
+  const getText = async () => {
+    try {
+      const response = await fetch('http://localhost:3000/private/test', {
+        method: 'get',
+        headers: {
+          'content-type': 'application/json',
+          authorization: localStorage.getItem('token'),
+        },
+      });
+      if (!response.ok) throw Error;
+      const data = await response.json();
+      setMessage(data.message);
+    } catch (error) {
+      console.log('error', error);
+      setMessage(null);
+    }
+  };
+  useEffect(() => {
+    if (currUser) { getText(); }
+  }, [currUser]);
+  return (
+    <div>{message}</div>
+  );
+};
+export default PrivateText;

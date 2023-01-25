ffe9833bfa0e9d50a3afd6aea5ea2354ab4e5564
dgonzalesi <dgonzalesi@comunidad.unam.mx>
2023-01-25 11:57:25 -0600

    Init install redux

--- src/index.js
+++ src/index.js
@@ -3,13 +3,17 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import store from './redux/store';
 import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <BrowserRouter>
-      <App />
+      <Provider configureStore={store}>
+        <App />
+      </Provider>
     </BrowserRouter>
   </React.StrictMode>,
 );

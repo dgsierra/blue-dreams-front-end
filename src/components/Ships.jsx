a8bfb78f27ed999895b2b4858ecefbb6f7f408c3
dgonzalesi <dgonzalesi@comunidad.unam.mx>
2023-01-26 14:07:23 -0600

    Add ships data to the redux

--- src/components/Ships.jsx
+++ src/components/Ships.jsx
@@ -1,6 +1,10 @@
 import React from 'react';
+import { useFetchShipsQuery } from '../redux/slices/ships-slice';
 
 export default function Ships() {
+  const { data: ships, isFetching } = useFetchShipsQuery();
+  console.log('ships', ships);
+  console.log('isFetching', isFetching);
   return (
     <div>Ships The Component </div>
   );
--- src/redux/store.js
+++ src/redux/store.js
@@ -3,11 +3,14 @@ import { configureStore } from '@reduxjs/toolkit';
 import reservation from './reducer/reservation';
 import user from './reducer/user';
 import ship from './reducer/ship';
+import { shipsApi } from './slices/ships-slice';
 
 export default configureStore({
   reducer: {
     reservation,
     user,
     ship,
+    [shipsApi.reducerPath]: shipsApi.reducer,
   },
+  middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(shipsApi.middleware),
 });

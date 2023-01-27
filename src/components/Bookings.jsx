9fe90876b7b1b4c7928ad85e95b853a586a24ca6
dgonzalesi <dgonzalesi@comunidad.unam.mx>
2023-01-27 01:30:22 -0600

    Fix linters

--- src/components/Bookings.jsx
+++ src/components/Bookings.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable max-len */
 import React from 'react';
 import './Booking.css';
 
@@ -12,7 +13,7 @@ export default function Bookings() {
         </ul>
         <ul className="ms-5 list-unstyled text-light">
           <li>1. All rental boats must be returned in the same condition as when they were rented.</li>
-          <li>2. Renters must have a valid boater's license and provide proof of insurance.</li>
+          <li>2. Renters must have a valid boaters license and provide proof of insurance.</li>
           <li>3. Renters must be at least 18 years of age and provide a valid government-issued ID.</li>
           <li>4. Renters must review and sign a rental agreement prior to taking possession of the boat.</li>
           <li>5. Renters are responsible for any damages or losses incurred during the rental period.</li>
@@ -23,7 +24,7 @@ export default function Bookings() {
           <li>10. Renters are responsible for following all applicable laws and regulations while operating the rental boat.</li>
           <li className="mt-1">Procedure:</li>
           <li>1. Customers must make a reservation in advance to rent a boat.</li>
-          <li>2. Upon arrival, customers must present a valid government-issued ID and boater's license for verification.</li>
+          <li>2. Upon arrival, customers must present a valid government-issued ID and boaters license for verification.</li>
           <li>3. Customers must review and sign the rental agreement.</li>
           <li>4. Customers will receive a brief orientation on the operation of the boat and safety procedures.</li>
           <li>5. Customers will be provided with all necessary safety equipment, including life jackets.</li>
@@ -34,21 +35,21 @@ export default function Bookings() {
           <li>10. Customers will be provided with a receipt for their rental.</li>
         </ul>
         <div className="text-center">
-          <a type="button" className="btn navbar-toggler bg-dark p-2 text-decoration-none link-light mb-4" data-bs-toggle="modal" data-bs-target="#staticBackdrop">
+          <button type="button" className="btn navbar-toggler bg-dark p-2 text-decoration-none link-light mb-4" data-bs-toggle="modal" data-bs-target="#staticBackdrop">
             BOOK NOW
-          </a>
+          </button>
         </div>
         <div className="modal fade" id="staticBackdrop" data-bs-backdrop="static" data-bs-keyboard="false" tabIndex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
           <div className="modal-dialog">
             <div className="modal-content bg2">
               <div className="modal-header">
                 <h5 className="modal-title text-light" id="staticBackdropLabel">BOOKING</h5>
-                <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close" />
               </div>
               <form>
                 <fieldset className="p-5">
                   <div className="mb-3">
-                    <label for="disabledSelect" className="form-label text-light">Ships</label>
+                    <label htmlFor="disabledSelect" className="form-label text-light">Ships</label>
                     <select id="disabledSelect" className="form-select">
                       <option>Select</option>
                       <option>YT-H5</option>
@@ -58,7 +59,7 @@ export default function Bookings() {
                     </select>
                   </div>
                   <div className="mb-3">
-                    <label for="Select" className="form-label text-light">City</label>
+                    <label htmlFor="Select" className="form-label text-light">City</label>
                     <select id="Select" className="form-select">
                       <option>Select</option>
                       <option>Cartagena</option>
@@ -68,26 +69,26 @@ export default function Bookings() {
                     </select>
                   </div>
                   <div className="mb-3">
-                    <label for="Select" className="bg form-label text-light">Start Date</label>
+                    <label htmlFor="Select" className="bg form-label text-light">Start Date</label>
                     <div className="input-group date" id="datepicker">
                       <input type="text" className="form-control" />
                       <span className="input-group-append">
                         <span className="input-group-text bg-white">
-                          <i className="fa fa-calendar"></i>
+                          <i className="fa fa-calendar" />
                         </span>
                       </span>
                     </div>
-                    <label for="disabledSelect" className="form-label mt-3 text-light">End Date</label>
+                    <label htmlFor="disabledSelect" className="form-label mt-3 text-light">End Date</label>
                     <div className="input-group date" id="datepicker">
                       <input type="text" className="form-control" />
                       <span className="input-group-append">
                         <span className="input-group-text bg-white">
-                          <i className="fa fa-calendar"></i>
+                          <i className="fa fa-calendar" />
                         </span>
                       </span>
                     </div>
                     <div className="text-center d-flex justify-content-center row">
-                      <label for="Select" className="form-label mt-3 text-light">Cost</label>
+                      <label htmlFor="Select" className="form-label mt-3 text-light">Cost</label>
                       <card type="display" className="form-control form1 p-3 w-50" />
                     </div>
                   </div>
